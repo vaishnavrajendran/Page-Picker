a5c25c5c803b79d8e093c87aac065d88da26c727
vaishnavrajendran <vaishnavrajendran95@gmail.com>
2023-11-20 22:57:34 +0530

    first commit

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from 'next/font/google'
+import { Open_Sans } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const open_sans = Open_Sans({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={open_sans.className}>{children}</body>
     </html>
   )
 }
